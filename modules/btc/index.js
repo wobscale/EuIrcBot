3a4b88fc1b22eaaddb45b83b11bf73989df39b43
Euan <euank@euank.com>
2013-12-07 16:43:14 -0500

    BTC Error handling

--- modules/btc/index.js
+++ modules/btc/index.js
@@ -4,7 +4,7 @@ module.exports.command = "btc";
 
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw) {
   btc.price('mtgox', function(err, prices) {
-    if(err) return reply("Error occured getting BTC prices");
+    if(err || prices === undefined || prices.data === undefined) return reply("Error occured getting BTC prices");
     if(prices.data[remainder]) {
       reply(remainder + ":", prices.data[remainder].display);
     } else {
