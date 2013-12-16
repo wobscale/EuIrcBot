d7bea5e2fbbb1b0eb34f0d759cc5a911206a32ec
Euank <euank@clemson.edu>
2013-12-16 18:18:42 -0500

    Cleanup btc/ltc command

--- modules/btc/index.js
+++ modules/btc/index.js
@@ -1,27 +1,24 @@
 var BTCE = require('btce');
 var btce = new BTCE();
 
-module.exports.commands = {
-  "btc": function(remainder, parts, reply, command, from, to, text, raw) {
-    btce.ticker({pair: "btc_usd"}, function(err, data) {
-      if(err || !data || !data.ticker) return reply("Error occured getting BTC prices");
-      data = data.ticker;
-      if(data[remainder.toLowerCase()]) {
-        reply(remainder + ":", data[remainder.toLowerCase()]);
-      } else {
-        reply("Last:", data.last, "| Avg:", data.avg);
-      }
-    });
-  },
-  "ltc": function(remainder, parts, reply, command, from, to, text, raw) {
-    btce.ticker({pair: "ltc_usd"}, function(err, data) {
-      if(err || !data || !data.ticker) return reply("Error occured getting BTC prices");
-      data = data.ticker;
-      if(data[remainder.toLowerCase()]) {
-        reply(remainder + ":", data[remainder.toLowerCase()]);
-      } else {
-        reply("Last:", data.last, "| Avg:", data.avg);
-      }
-    });
-  },
+function capitalize(str) {
+  if(str === '') return '';
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function getTickerData(coin, metric, callback) {
+  btce.ticker({pair: coin+"_usd"}, function(err, data) {
+    if(err || !data || !data.ticker) return callback("Error occured getting BTC prices");
+    data = data.ticker;
+    if(data[metric.toLowerCase()]) {
+      callback(capitalize(metric) + ": $" + data[metric.toLowerCase()]);
+    } else {
+      callback("Last: $" + data.last + " | Avg: $" + data.avg);
+    }
+  });
+}
+
+module.exports.commands = ['btc', 'ltc'];
+module.exports.run = function(remainder, parts, reply, command, from, to, text, raw) {
+  getTickerData(command, remainder, reply);
 };
