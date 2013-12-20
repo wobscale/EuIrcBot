b8456e28de43cc1e6344e3e7d9eafe56f8136fa0
Euan <euank@euank.com>
2013-12-19 22:53:39 -0500

    Remove debug logs

--- modules/twitter/index.js
+++ modules/twitter/index.js
@@ -7,14 +7,10 @@ var twitRegex = /^(https?\:\/\/)?(www\.)?twitter\.com\/([a-zA-Z0-9_]+)(\/status\
 
 module.exports.url = function(url, reply) {
   var m;
-  console.log("Trying " + url);
   if((m = twitRegex.exec(url))) {
-    console.log(m);
     if(m[3] && !m[5]) {
       // User page
-      console.log(m[3]);
       t.get("/users/show", {screen_name: m[3]}, function(err, res) {
-        console.log(err);
         if(err) reply("Error getting user " + m[3]);
         else reply(res.name + ": " + res.description);
       });
