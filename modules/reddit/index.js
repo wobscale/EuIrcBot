e7f6da8d9257afaaad1e37aa5ef74201f0b0ee52
LinuxMercedes <linuxmercedes@gmail.com>
2014-02-19 15:15:37 -0600

    Untested reddit comment code

---
+++ modules/reddit/index.js
@@ -0,0 +1,23 @@
+var S = requre('string');
+var http = require('http');
+
+module.exports.url = function(url, reply) {
+	http.get(url + '.json', function(res) {
+		var data = '';
+		res.on('data', function(chunk) {
+			data += chunk;
+		});
+		res.on('end', function() {
+			try {
+				var json = JSON.parse(data);
+				var comment_json = json[1]['data']['children']['data']['body_html'];
+				var comment = S(comment_json).stripTags().decodeHTMLEntities().s //For some reason functions are applied right-to-left
+				
+				console.log(comment);
+				reply(comment);
+			} catch(e) {
+				return reply("Error handling response");
+			}
+		});
+	});
+};
