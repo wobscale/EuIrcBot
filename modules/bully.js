9902c591589fe5f20133845ccf4d7631ce173cab
Euan <euank@euank.com>
2014-05-01 10:45:23 -0400

    Allow multi-bullying

--- modules/bully.js
+++ modules/bully.js
@@ -1,4 +1,4 @@
 module.exports.command = "bully";
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw) {
-  reply(remainder + ": I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
+  reply(parts.join(", ") + ": I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
 };
