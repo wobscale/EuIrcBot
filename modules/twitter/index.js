aaabd2c9f1e4fe11aca962297adbac062dabb68b
Euan <euank@euank.com>
2014-05-04 19:18:02 -0400

    Fix twitter onUrl

--- modules/twitter/index.js
+++ modules/twitter/index.js
@@ -5,29 +5,35 @@ var t = null;
 
 var twitRegex = /^(https?\:\/\/)?(www\.)?twitter\.com\/([a-zA-Z0-9_]+)(\/status\/(\d+))?/;
 
-module.init = function(bot) {
+module.exports.init = function(bot) {
   bot.getConfig("twitter.json", function(err, conf) {
     if(err) bot.say("Unable to load twitter module: " + err);
-    t = new Twit(conf);
+    try {
+      t = new Twit(conf);
+    } catch(ex) {
+      bot.say("Error loading twitter library: " + ex);
+    }
   });
 };
 
 module.exports.url = function(url, reply) {
-  if(t === null) return;
+  if(t === null) return reply("Unable to handle twitter url; lib not loaded");
+
   var m;
   if((m = twitRegex.exec(url))) {
     if(m[3] && !m[5]) {
       // User page
       t.get("/users/show", {screen_name: m[3]}, function(err, res) {
         if(err) reply("Error getting user " + m[3]);
-        else reply(ent.decode(res.name) + ": " + ent.decode(res.description));
+        else reply(ent.decode(res.name) + " (@" + ent.decode(res.screen_name) + "): " + ent.decode(res.description));
       });
     } else {
       var uname = m[3];
       var id = m[5];
       t.get("/statuses/show/:id", {id: id}, function(err, res) {
+        console.log(res.user);
         if(err) reply("Error getting tweet");
-        else reply(ent.decode(res.user.name) + ": " + ent.decode(res.text));
+        else reply(ent.decode(res.user.name) + " (@" + ent.decode(res.user.screen_name) + "): " + ent.decode(res.text));
       });
     }
   }
