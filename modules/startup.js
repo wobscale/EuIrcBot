7536c6bea6ee7365fada4508a6c2d40a20da093b
Euan <euank@euank.com>
2014-05-04 20:16:15 -0400

    More startup ideas

--- modules/startup.js
+++ modules/startup.js
@@ -7,7 +7,10 @@ module.exports.run = function(remainder, parts, reply, command, from, to, text, 
     'Maps', 'Quora', 'Groupon', 'RSS', 'Redis', 'MapReduce', 'the iPhone',
     'Weedmaps', 'SnapChat', 'drones', 'WordPress', 'Napster', 'Tumblr', 'AirBnB',
     'cloud storage', 'a jQuery plugin', 'Reddit', 'Cash4Gold', 'Flickr', 'dongs',
-    'Wikipedia', 'eHarmony', 'Amazon', 'Adult Friend Finder', 'EC2', 'S3'
+    'Wikipedia', 'eHarmony', 'Amazon', 'Adult Friend Finder', 'EC2', 'S3',
+    '4chan', 'Blogger', 'Pinterest', 'The PS3', 'The XBOX', 'Craigslist', 'XHamster',
+    'CNN', 'A new Programming language', 'A Webbrowser', 'Ads', 'The Pirate Bay', 'Ebay',
+    'Deviant Art', 'Mail order bombs'
   ];
   
   var markets = [
@@ -17,7 +20,11 @@ module.exports.run = function(remainder, parts, reply, command, from, to, text, 
     'shopping', 'cougars', 'MILFs', 'good looking people', 'the army', 'interns',
     'whiskey', 'retirement planning', 'beer', 'traveling', 'funerals', 'dongs',
     'presentations', 'online food delivery', 'the homeless', 'managing exes',
-    'small businesses', 'coffee', 'textbooks', 'politics', 'your mom'
+    'small businesses', 'coffee', 'textbooks', 'politics', 'your mom',
+    'gay people', 'cats', 'dogs', 'Germans', 'Christians', 'Athiests',
+    'redditors', 'guns', 'ladies', 'men', 'programmers', 'ninjas', 'pirates',
+    'criminals', 'prisoners', 'autists', 'artists', 'emo teens', 'babies',
+    'republicans'
   ];
   
   var chosen_service = services[Math.floor(Math.random() * services.length)],
