9bbbc7d4da0252ac5b60f7562dd41e7195ba635b
Graham Mackie <mackieg@umich.edu>
2014-05-20 23:45:00 -0400

    cleaned up module, added some error checking

--- modules/weather.js
+++ modules/weather.js
@@ -1,13 +1,24 @@
 var http = require('http');
 var https = require('https');
+var config;
 
 module.exports.commands = ["w", "weather"];
+
+module.exports.init = function(bot) {
+  bot.getConfig( "weather.json", function( err, conf ) {
+    if( err ) {
+      console.log( err );
+    } else {
+      config = conf;
+    }
+  });
+};
 
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw) {
   var str = remainder.replace(/ /g, '+');
 
   https.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + str 
-+'&sensor=false&key=AIzaSyAmHJlkmm4TjqyqISo9LfYGK7NoJSoCFtc', function(res) {
++'&sensor=false&key=' + config.google_key, function(res) {
     var data = '';
     res.on('data', function(chunk) {
       data += chunk;
@@ -20,52 +31,57 @@ module.exports.run = function(remainder, parts, reply, command, from, to, text, 
         return reply("Error handling response");
       }
 
-      if ( locData.cod == 200 ){
-        var address_components = locData.results.address_components;
-        var city;
-        var state;
-        var i;
-        for (i = 0; i < address_components.length; ++i)
+      if (typeof(locData.results[0]) === "undefined" )
+        return reply("Error: Couldn't find city/state");
+      var address_components = locData.results[0].address_components;
+      var city;
+      var state;
+      var country;
+      var i;
+      for (i = 0; i < address_components.length; ++i)
+      {
+        if (address_components[i].types.indexOf("country") != -1)
         {
-          if (address_components[i].types.indexOf("locality") != -1)
-          {
-            city = address_components[i].short_name;
-          }
-          if (address_components[i].types.indexOf("administrative_area_level_1") != -1)
-          {
-            state = address_components[i].component.short_name;
-          }
+          country = address_components[i].short_name;
         }
-        city = city.replace(/ /g, '_');
-        var locStr = state + "/" + city;
-        http.get('http://api.wunderground.com/api/e2749beacc546174/conditions/q/'
-          + locStr + '.json', function(res) {
-          var wudata = '';
-          res.on('data', function(chunk) {
-          wudata += chunk;
-          });
-          var weatherData;
-          result.on('end', function() {
-            try {
-              weatherData = JSON.parse(wudata.toString());
-            } catch(e) {
-              return reply("Error handling response");
-            }
 
-            if ( weatherData.cod == 200 ){
-              var conditions = weatherData.current_observation;
-              reply(conditions.display_location.full + ' | ' + conditions.temp_f +
-                '°F, '  + conditions.weather + ' | Feels Like: ' +
-                conditions.feelslike_f + '°F');
-            }     
-            else{
-              reply("Could not get weather for: " + locStr);
-            }
-          });
-        });
-      }else{
-        reply("Could not find: " + remainder);
+        if (address_components[i].types.indexOf("locality") != -1)
+        {
+          city = address_components[i].short_name;
+        }
+        
+        if (address_components[i].types.indexOf("administrative_area_level_1") != -1)
+        {
+          state = address_components[i].short_name;
+        }
       }
+      if (country != "US")
+        return reply("Error: Location is not in the United States!");
+      if (typeof(city) === "undefined" || typeof(state) === "undefined")
+        return reply("Error: Couldn't find city/state");
+
+      city = city.replace(/ /g, '_');
+      var locStr = state + "/" + city;
+      http.get('http://api.wunderground.com/api/' + config.wunderground_key 
+        + '/conditions/q/' + locStr + '.json', function(res) {
+        var wudata = '';
+        res.on('data', function(chunk) {
+        wudata += chunk;
+        });
+        var weatherData;
+        res.on('end', function() {
+          try {
+            weatherData = JSON.parse(wudata.toString());
+          } catch(e) {
+            return reply("Error handling response");
+          }
+
+          var conditions = weatherData.current_observation;
+          reply(conditions.display_location.full + ' | ' + conditions.temp_f +
+            '°F, '  + conditions.weather + ' | Feels Like: ' +
+            conditions.feelslike_f + '°F');
+        });
+      });
     });
   });
 };
