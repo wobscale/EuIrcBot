03069a80c2f306cb616016211d45c26d663f5870
Euan <euank@euank.com>
2014-06-11 19:41:25 -0400

    Fix refactoring typo

--- modules/bully.js
+++ modules/bully.js
@@ -2,7 +2,7 @@
 
 module.exports.run_bully = function(r, p, reply) {
 
-  reply(parts.join(", ") +
+  reply(p.join(", ") +
         ": I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
 
 };
