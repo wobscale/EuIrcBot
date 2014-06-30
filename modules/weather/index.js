403364471df6c99c0c3949454aad8942777cbe0e
Graham Mackie <mackieg@umich.edu>
2014-06-30 16:59:19 -0400

    refactored module into one file, easy to add other wunderground api calls. Also added support for puerto rico!

---
+++ modules/weather/index.js
@@ -0,0 +1,139 @@
+var http = require('http');
+var https = require('https');
+var config;
+
+module.exports.commands = ["forecast", "w", "weather", "daily"];
+
+module.exports.init = function(bot) {
+  bot.getConfig( "weather.json", function( err, conf ) {
+    if( err ) {
+      console.log( err );
+    } else {
+      config = conf;
+    }
+  });
+};
+
+module.exports.run = function(remainder, parts, reply, command, from, to, text, raw){
+  var locStr;
+  locStr = remainder.replace(/ /g, '+');
+
+  https.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locStr 
+  +'&sensor=false&key=' + config.google_key, function(res) {
+    var data = '';
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+    var locData;
+    res.on('end', function() {
+      try {
+        locData = JSON.parse(data.toString());
+      } catch(e) {
+        return reply("Error handling response");
+      }
+
+      if (typeof(locData.results[0]) === "undefined" )
+        return reply("Error: Couldn't find city/state");
+      var address_components = locData.results[0].address_components;
+      var city;
+      var state;
+      var country;
+      var i;
+      for (i = 0; i < address_components.length; ++i)
+      {
+        if (address_components[i].types.indexOf("country") != -1)
+        {
+          country = address_components[i].short_name;
+        }
+
+        if (address_components[i].types.indexOf("locality") != -1)
+        {
+          city = address_components[i].short_name;
+        }
+        
+        if (address_components[i].types.indexOf("administrative_area_level_1") != -1)
+        {
+          state = address_components[i].short_name;
+        }
+      }
+      
+      if (country != "US" && country != "PR")
+        return reply("Error: Location is not in the United States!");
+      if (typeof(city) === "undefined" || typeof(state) === "undefined")
+        return reply("Error: Couldn't find city/state");
+
+      city = city.replace(/ /g, '_');
+      if (country == "PR")
+        locStr = country + "/" + city;
+      else
+        locStr = state + "/" + city;
+
+      switch(command)
+      {
+        case "w":
+          return getWeather(locStr, reply);
+        case "weather":
+          return getWeather(locStr, reply);
+        case "forecast":
+          return getForecast(locStr, reply);
+        case "daily":
+          break;
+        default:
+          return reply("Error, command <" + command + "> not implemented!");
+      }
+
+    });
+  });
+}
+
+function getWeather(locStr, reply) {
+  http.get('http://api.wunderground.com/api/' + config.wunderground_key 
+      + '/conditions/q/' + locStr + '.json', function(res) {
+    var wudata = '';
+    res.on('data', function(chunk) {
+      wudata += chunk;
+    });
+    var weatherData;
+    res.on('end', function() {
+      try {
+        weatherData = JSON.parse(wudata.toString());
+      } catch(e) {
+        return reply("Error handling response");
+      }
+
+      var conditions = weatherData.current_observation;
+      reply(conditions.display_location.full + ' | ' + conditions.temp_f +
+        '°F, '  + conditions.weather + ' | Humidity: ' +
+        conditions.relative_humidity);
+   });
+ });
+}
+
+
+function getForecast(locStr, reply) {
+  http.get('http://api.wunderground.com/api/' + config.wunderground_key 
+      + '/forecast/q/' + locStr + '.json', function(res) {
+    var wudata = '';
+    res.on('data', function(chunk) {
+      wudata += chunk;
+    });
+    var weatherData;
+    res.on('end', function() {
+      try {
+        weatherData = JSON.parse(wudata.toString());
+      } catch(e) {
+        return reply("Error handling response");
+      }
+
+      var forecast = weatherData.forecast.simpleforecast.forecastday;
+      for(i = 0; i < forecast.length; ++i)
+      {
+        reply(forecast[i].date.weekday + ' ' + forecast[i].date.month + '/' +
+          forecast[i].date.day + ': ' + forecast[i].conditions + ' | High: ' +
+          forecast[i].high.fahrenheit +  '°F  | Low: ' +
+          forecast[i].low.fahrenheit + '°F | Precipitation: ' +
+          forecast[i].pop + '%');
+     }
+   });
+ });
+}
