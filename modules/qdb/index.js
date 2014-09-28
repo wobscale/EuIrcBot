44a19fef87f3be668a524c3e40c1f3e3ab083e9a
ichivack <ichivack@gmail.com>
2014-09-28 16:35:20 -0700

    Make qdb work with a different qdb software

--- modules/qdb/index.js
+++ modules/qdb/index.js
@@ -1,14 +1,21 @@
-var http = require('http');
+var http = require('http'),
+    requestify = require('requestify');
 module.exports.commands = ['quo', 'qudb', 'quodb', 'qdb'];
 
 var bot;
+var conf = null;
 
 module.exports.init = function(b) {
   bot = b;
+  bot.getConfig("qdb.json", function(err, co) {
+    if(err) console.log("Error with QDB module, no conf");
+    else conf = co;
+  });
 };
 
 module.exports.run = function(rem, parts, reply, command, from, to, text, raw) {
   if(to[0] != '#' && to[0] != '&') return;
+  if(conf === null) return;
 
   var scrollbackModule = bot.modules['sirc-scrollback'];
 
@@ -17,10 +24,17 @@ module.exports.run = function(rem, parts, reply, command, from, to, text, raw) {
   scrollbackModule.getFormattedScrollbackLinesFromRanges(to, parts, function(err, res) {
     if(err) return reply(err);
 
-    http.get('http://qdb.amazdong.com/new?text=' + encodeURIComponent(res), function(res) {
-      // All good
-    }).on('error', function(e) {
-      reply("Error qdbing - " + e);
+    requestify.post(conf.baseUrl + '/api/quote', {
+      quote: res,
+      source: conf.source
+    }).then(function(response) {
+      // Success
+      var id = 'unknown';
+      try {
+        id = JSON.parse(response.body).id;
+      } catch(e){}
+
+      bot.sayTo(from, conf.baseUrl + "/#/quote/" + id);
     });
   });
 };
