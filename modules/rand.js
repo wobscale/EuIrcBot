4ac57ae188623b7fc64c81f147b2dcf927c78c0c
LinuxMercedes <linuxmercedes@gmail.com>
2014-10-15 16:18:44 -0500

    Half of a multirand implememtnation
    Unfortunately javascript makes this kind of gross because maps aren't
    really well implemented. So I'm just going to wait until ruby modules
    happen and reimplement.

--- modules/rand.js
+++ modules/rand.js
@@ -1,5 +1,27 @@
-exports.command = "rand";
 
-exports.run = function(r, parts, reply) {
-  reply(parts[Math.floor(Math.random() * parts.length)]);
+module.exports.commands = {
+	rand: function(r, parts, reply) {
+		reply(parts[Math.floor(Math.random() * parts.length)]);
+	}
+	multirand: function(r, parts, reply) {
+		var results = {};
+		console.log(parts);
+		for(var i = 0; i < parts[0]; i++) {
+			var choice = parts[Math.floor(Math.random() * parts.length)];
+			if(typeof results[choice] == "undefined") {
+				results[choice] = 1;
+			}
+			else {
+				results[choice] = results[choice] + 1;
+			}
+		}
+
+// Iterate over map by sorted values and print or whatever
+// python:
+// for (thing, count) in sorted(results.items(), key=lambda x: x[1]):
+//   print(thing + " " + count)
+//
+// apparently this is a hard problem in computer science
+
 };
+
