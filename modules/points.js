519967a0f0be7da9fff464bd96279d441a513367
Andrew Brown <ajbpn7@mst.edu>
2015-02-05 22:08:36 -0600

    top 10 and per-user score lookup

--- modules/points.js
+++ modules/points.js
@@ -32,14 +32,36 @@ module.exports.msg = function(text, from, reply, raw) {
 
 module.exports.commands = {
 
-	top10: function(r, parts, reply) {
-		reply("top 10 is:");
-	},
+  top10: function(r, parts, reply) {
+    var users_and_scores = [],
+      rank = 10;
 
-	score: function(r, parts, reply) {
-	  var user = parts[0];
-	  
-    reply("random reason from " + user);
-	}
+    // yolo js
+    for (var user in this.scoreboard) {
+      users_and_scores.append([user, scoreboard[user]['points']]);
+    }
 
+    // Sort pairs by score: [["dru", 4], ["suroi", 53]]
+    users_and_scores = users_and_scores.sort(function (a, b) {
+    	return a[1] <= b[1];
+    });
+
+    for (var line_data in users_and_scores) {
+    	reply("" + (11 - rank--) + ". " + line_data[0] + ": " + line_data[1] + " points!");
+    	if (rank == 0) { break; }
+    }
+  },
+
+  score: function(r, parts, reply) {
+    var user = parts[0],
+      points = 0;
+    
+    if (this.scoreboard[user]) {
+    	points = this.scoreboard[user]['points'];
+    }
+
+    reply(user + " has "  + points + " points!");
+    // #todo point reasons
+  }
+ 
 };
