13377c01fe03be8c2568c06d2c1d96bb928296a0
euank <euank@euank.com>
2015-03-10 23:11:25 -0700

    Improve number&plus filter regexes

--- modules/onMath/index.js
+++ modules/onMath/index.js
@@ -46,10 +46,10 @@ function constructMathRe() {
 var mathRe = constructMathRe();
 
 var onlySymbols = new RC("^[\\s" + REEscape(mathSymbols) + "]*$");
-var onlyNumbers = new RC(/^[\s\d]*$/);
+var onlyNumbers = new RC(/^[\.\s\d]*$/);
 var onlyKeys = new RC("^\\s*((" + mathKeys.join(")|(") + "))+\\s*$");
 var funnyFractions = new RC(/^\s*([0-9][0]?\s*\/\s*(10|5|100))\s*$/);
-var plusN = new RC("^\s*\+\d+\s*$");
+var plusN = new RC(/^\s*\++\d+\s*$/);
 
 var ignoreRe = onlySymbols.or(onlyNumbers).or(funnyFractions).or(onlyKeys).or(plusN);
 
