b686ac38d6000cb1bdf012151f04355900fb2ad0
LinuxMercedes <LinuxMercedes@gmail.com>
2015-03-23 15:33:50 -0500

    karma viewing commands

--- modules/karma.js
+++ modules/karma.js
@@ -79,9 +79,9 @@ module.exports.msg = function(text, from, reply, raw) {
 
 module.exports.commands = {
 
-  top10: function(r, parts, reply) {
+  top3: function(r, parts, reply) { // todo: use regex parsed commands to implement topN
     var users_and_scores = [],
-      rank = 10;
+    rank = 1;
 
     // yolo js
     for (var user in this.scoreboard) {
@@ -90,25 +90,28 @@ module.exports.commands = {
 
     // Sort pairs by score: [["dru", 4], ["suroi", 53]]
     users_and_scores = users_and_scores.sort(function (a, b) {
-    	return a[1] <= b[1];
+      return a[1] <= b[1];
     });
 
-    for (var line_data in users_and_scores) {
-    	reply("" + (11 - rank--) + ". " + line_data[0] + ": " + line_data[1] + " points!");
-    	if (rank == 0) { break; }
+    var output = "";
+    for (var line_data in users_and_scores.slice(0,2)) {
+      output += line_data[0] + ": " + line_data[1] + ", ");
     }
+    output += "\b\b";
+
+    reply(output);
   },
 
-  score: function(r, parts, reply) {
+  karma: function(r, parts, reply, command, from) {
     var user = parts[0],
-      points = 0;
-    
-    if (this.scoreboard[user]) {
-    	points = this.scoreboard[user]['points'];
+    points = 0;
+
+    if (this.scoreboard[from][user]) {
+      points = this.scoreboard[from][user]['points'];
     }
 
-    reply(user + " has "  + points + " points!");
-    // #todo point reasons
+    reply(user + " has "  + points + " karma");
+    // todo: point reasons
   }
- 
+
 };
