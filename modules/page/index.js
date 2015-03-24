1592fa15b3e81fda3fe804fc35f0af0c26ce133f
LinuxMercedes <LinuxMercedes@gmail.com>
2015-03-24 09:09:38 -0500

    Indenting

--- modules/page/index.js
+++ modules/page/index.js
@@ -7,31 +7,31 @@ var addresses = {};
 var ses = null;
 
 function getAddresses(channel, cb) {
-	bot.readDataFile(channel + '.json', function(err, res) {
-		if(err) {
-			console.log("no data");
-			addresses[channel] = {};
-			cb();
-		}
-		else {
-			addresses[channel] = JSON.parse(res);
-			cb();
-		}
-	});
+  bot.readDataFile(channel + '.json', function(err, res) {
+    if(err) {
+      console.log("no data");
+      addresses[channel] = {};
+      cb();
+    }
+    else {
+      addresses[channel] = JSON.parse(res);
+      cb();
+    }
+  });
 }
 
 function saveAddresses(channel) {
-	bot.writeDataFile(channel + '.json', JSON.stringify(addresses[channel]), function(err) {
-		if(err)console.log("Error saving addresses for channel " + channel);
-	});
+  bot.writeDataFile(channel + '.json', JSON.stringify(addresses[channel]), function(err) {
+    if(err)console.log("Error saving addresses for channel " + channel);
+  });
 }
 
 function sendMailTo(user, channel, text, reply) {
-	getAddresses(channel, function() {
+  getAddresses(channel, function() {
 
-		if(!addresses[channel] || !addresses[channel][user]) {
-			reply("Who the heck is that");
-		}
+    if(!addresses[channel] || !addresses[channel][user]) {
+      reply("Who the heck is that");
+    }
 
     if(ses) {
       ses.sendEmail({
@@ -72,35 +72,35 @@ function sendMailTo(user, channel, text, reply) {
         reply("beep boop beep boop");
       });
     }
-	});
+  });
 }
 
 
 module.exports.init = function(b) {
-	bot = b;
-	config = bot.getConfig("page.json", function(err, conf) {
-		if(err == null) {
-			config = conf;
-			transporter = nodemailer.createTransport(smtpTransport(config));
+  bot = b;
+  config = bot.getConfig("page.json", function(err, conf) {
+    if(err == null) {
+      config = conf;
+      transporter = nodemailer.createTransport(smtpTransport(config));
 
-			if(config['accessKeyId']) {
-				ses = new aws.SES(config);
-			}
+      if(config['accessKeyId']) {
+        ses = new aws.SES(config);
+      }
 
-			transporter.on('log', function(log) {
-				console.log(log);
-			});
+      transporter.on('log', function(log) {
+        console.log(log);
+      });
 
-		}
-		else {
-			console.err("Can't get pager config");
-		}
-	});
+    }
+    else {
+      console.err("Can't get pager config");
+    }
+  });
 }
 
 module.exports.commands = {
-	addpager: function(r, p, reply, command, from, channel) {
-		if(p.length >= 1) {
+  addpager: function(r, p, reply, command, from, channel) {
+    if(p.length >= 1) {
       getAddresses(channel, function() {
         if(ses) {
           ses.verifyEmailIdentity({'EmailAddress': p[0]}, function(err, data) {
@@ -120,18 +120,18 @@ module.exports.commands = {
           reply("Added address " + p[0] + " for user " + from);
         }
       });
-		}
-		else {
-			reply("you didn't tell me enough stuff, doofus");
-		}
-	},
+    }
+    else {
+      reply("you didn't tell me enough stuff, doofus");
+    }
+  },
 
-	page: function(r, p, reply, command, from, channel) {
-		if(p.length >= 2) {
-			sendMailTo(p[0], channel, p.slice(1).join(" "), reply);
-		}
-		else {
-			reply("Yah I don't know what that means");
-		}
-	}
+  page: function(r, p, reply, command, from, channel) {
+    if(p.length >= 2) {
+      sendMailTo(p[0], channel, p.slice(1).join(" "), reply);
+    }
+    else {
+      reply("Yah I don't know what that means");
+    }
+  }
 }
