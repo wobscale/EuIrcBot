97de5fcbe26a6f538541ea43a1505ba7da764a40
LinuxMercedes <LinuxMercedes@gmail.com>
2015-03-24 09:09:29 -0500

    SES integration

--- modules/page/index.js
+++ modules/page/index.js
@@ -1,8 +1,10 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
+var aws = require('aws-sdk');
 
-var config, bot, transporter;
+var config, bot, transporter;;
 var addresses = {};
+var ses = null;
 
 function getAddresses(channel, cb) {
 	bot.readDataFile(channel + '.json', function(err, res) {
@@ -31,18 +33,45 @@ function sendMailTo(user, channel, text, reply) {
 			reply("Who the heck is that");
 		}
 
-		transporter.sendMail({
-			from: config.from,
-			to: addresses[channel][user],
-			subject: config.subject,
-			text: text
-		}, function(error, info) {
-			if(error) {
-				reply(error);
-				return;
-			}
-			reply("beep boop beep boop");
-		});
+    if(ses) {
+      ses.sendEmail({
+        Destination:{
+          ToAddresses:[addresses[channel][user]]
+        },
+        Message:{
+          Subject:{
+            Data:config.subject
+          },
+          Body:{
+            Text:{
+              Data: text
+            }
+          }
+        },
+        Source:config.from,
+      },
+      function(err, data) {
+        if(err) {
+          reply(err);
+          return;
+        }
+        reply("beep boop beep boop");
+      });
+    }
+    else {
+      transporter.sendMail({
+        from: config.from,
+        to: addresses[channel][user],
+        subject: config.subject,
+        text: text
+      }, function(error, info) {
+        if(error) {
+          reply(error);
+          return;
+        }
+        reply("beep boop beep boop");
+      });
+    }
 	});
 }
 
@@ -53,6 +82,10 @@ module.exports.init = function(b) {
 		if(err == null) {
 			config = conf;
 			transporter = nodemailer.createTransport(smtpTransport(config));
+
+			if(config['accessKeyId']) {
+				ses = new aws.SES(config);
+			}
 
 			transporter.on('log', function(log) {
 				console.log(log);
@@ -68,12 +101,25 @@ module.exports.init = function(b) {
 module.exports.commands = {
 	addpager: function(r, p, reply, command, from, channel) {
 		if(p.length >= 1) {
-			getAddresses(channel, function() {
-				addresses[channel][from] = p[0];
-				saveAddresses(channel);
-			});
-
-			reply("Added address " + p[0] + " for user " + from);
+      getAddresses(channel, function() {
+        if(ses) {
+          ses.verifyEmailIdentity({'EmailAddress': p[0]}, function(err, data) {
+            if(err) {
+              console.log("SES email verify error " + err);
+            }
+            else {
+              addresses[channel][from] = p[0];
+              saveAddresses(channel);
+              reply("Added address " + p[0] + " for user " + from);
+            }
+          });
+        }
+        else {
+          addresses[channel][from] = p[0];
+          saveAddresses(channel);
+          reply("Added address " + p[0] + " for user " + from);
+        }
+      });
 		}
 		else {
 			reply("you didn't tell me enough stuff, doofus");
