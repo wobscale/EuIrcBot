1a1f8aa3b9e462c7ff3ceadbeafa3974bcdb51f4
Billy Rhoades <billyarhoades@gmail.com>
2015-08-11 11:32:02 -0400

    Now cleanly getting all results, page and content matches.

--- modules/wiki/index.js
+++ modules/wiki/index.js
@@ -15,17 +15,25 @@ module.exports.init = function(bot) {
 };
 module.exports.name = "sirc-wiki";
 
-module.exports.commands = ["wi", "wiki"];
+module.exports.commands = ["wi", "wiki", "wikis"];
 
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw, regex)
 {
   var requrl = config.base + "?do=search&id="
                + parts.map(encodeURIComponent).join('+');
-
-  getContent(reply, parts, requrl, searchWiki);
+  console.log("COMMAND: " + command);
+  if(command == "wikis" || command == "ws" || command == "wis"
+      || (command == "wiki" && parts.length > 1 && (parts[0] == "s" || parts[0] == "search" || parts[0] == "url")))
+  {
+    if(parts[0] == "s" || parts[0] == "search")
+    {
+      parts = parts.splice(1, parts.length);
+    }
+    getContent(reply, parts, requrl, searchWiki);
+  }
 };
 
-var getContent = function (reply, parts, page, callback)
+var getContent = function(reply, parts, page, callback)
 {
   request.post({ url: config.base, // logs us in
         'form': {
@@ -37,10 +45,10 @@ var getContent = function (reply, parts, page, callback)
         followAllRedirects: true,
         jar: true, //cookies
       }, 
-      function (err, httpResponse, body)
+      function(err, httpResponse, body)
       {
         request({ url: page, jar: true}, // gets the page
-          function (err, httpResponse, body)
+          function(err, httpResponse, body)
           {
             callback(reply, parts, body);
           }
@@ -53,23 +61,41 @@ var searchWiki = function(reply, parts, body)
 {
   jsdom.env(body, ["http://code.jquery.com/jquery.js"], function (err, window) {
     var $ = window.$;
-    var results = $(".search_quickhits > li");
+    var pageresults = $(".search_quickhits > li");
+    var contentresults = $(".search_results > dt");
 
-    if(results.length > 1)
+    // Exact page match, return it
+    if(pageresults.length == 1)
     {
-      reply(results.length + " results: " + config.base 
+      var match = $(".search_quickhits > li:nth-child(1) > a:nth-child(1)");
+      reply(createWikiURL(match.attr("href")));
+    }
+    else if(pageresults.length > 1)
+    {
+      reply(pageresults.length + " page results: " + config.base 
             + "?do=search&id=" + parts.map(encodeURIComponent).join("+"));
     }
-    else if(results.length == 0)
+    else if(contentresults.length == 1)
     {
-      reply("No results");
+      var match = $(".search_results > dt:nth-child(1) > a:nth-child(1)");
+      reply(createWikiURL(match.attr("href")));
+    }
+    else if(contentresults.length > 1)
+    {
+      reply(contentresults.length + " content results: " + config.base 
+            + "?do=search&id=" + parts.map(encodeURIComponent).join("+"));
     }
     else
     {
-      var uri = URI(config.base);
-      var match = $(".search_quickhits > li:nth-child(1) > a:nth-child(1)");
-      reply(uri.protocol() + "://" + uri.hostname() + match.attr("href"));
+      reply("No results");
     }
-  });
+  }
+);
+
+var createWikiURL = function(href)
+{
+  var uri = URI(config.base);
+  return(uri.protocol() + "://" + uri.hostname() + href);
+};
 
 };
