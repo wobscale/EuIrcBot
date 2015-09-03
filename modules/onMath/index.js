93ac4d4ecb71c4c59f772f0555b4b5f756010729
Billy Rhoades <billyarhoades@gmail.com>
2015-09-03 00:14:32 -0400

    Readded regex filters to onMath.
    Conflicts:
    	modules/onMath/index.js

--- modules/onMath/index.js
+++ modules/onMath/index.js
@@ -1,4 +1,5 @@
 var mathjs = require('mathjs');
+var RC = require('regex-chain');
 
 function MathScopeEval(str) {
   try {
@@ -9,19 +10,19 @@ function MathScopeEval(str) {
   }
 }
 
-var REEscape = function(s) {
-      return s.replace(/[\-\/\\^\$\*\+\?\.\(\)\|\[\]{}]/g, '\\$&');
-};
-
-// custom symbols
-mathjs.epsilon = 8.854187817*Math.pow(10,-12);
-mathjs.k = 8.987551787368*Math.pow(10,9);
-
-var mathKeysToGet = Object.getOwnPropertyNames(mathjs);
+var mathKeysToGet = Object.getOwnPropertyNames(Math);
 var mathItems = {};
 for(var i=0;i<mathKeysToGet.length;i++) {
-  mathItems[mathKeysToGet[i]] = mathjs[mathKeysToGet[i]];
+  mathItems[mathKeysToGet[i]] = Math[mathKeysToGet[i]];
 }
+mathItems.TAU = Math.PI * 2;
+mathItems.fact = function(num) {
+    var rval=1;
+    for (var i = 2; i <= num; i++) {
+        rval = rval * i;
+    }
+    return rval;
+};
 
 var mathKeys = Object.keys(mathItems);
 var mathSymbols = ".,*+-/()%=";
@@ -34,12 +35,16 @@ var onlyQuote = new RC(/^".+"$/);
 var funnyFractions = new RC(/^\s*(([0-9][0]?|11)\s*\/\s*(10|5|100))\s*$/);
 var plusN = new RC(/^\s*\++\d+\s*$/);
 
-var ignoreRe = onlySymbols.or(onlyNumbers).or(funnyFractions).or(onlyKeys).or(plusN)
-               .or(onlyQuote).or(onlyTime);
+var ignoreRe = onlySymbols.or(onlyNumbers).or(funnyFractions).or(onlyKeys).or(plusN).or(onlyQuote);
 
 
 module.exports.msg = function(text, from, reply, raw) {
+  if(ignoreRe.test(text)) {
+    return;
+  }
+
   var res = MathScopeEval(text);
+
   if(res !== null) {
     reply(res);
   }
