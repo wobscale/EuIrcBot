748648a0fcd00a585941efd491ab8b45ef83105e
Euan <euank@euank.com>
2015-11-01 16:47:45 -0800

    Allow quoing by regexps

--- modules/scrollback/index.js
+++ modules/scrollback/index.js
@@ -115,13 +115,27 @@ module.exports.formatLine = function(line) {
 module.exports.getFormattedScrollbackLinesFromRanges = function(channel, ranges, cb) {
   var linesToGet = [];
   var parts = ranges;
+  if(parts.length === 0 || parts.length === 1 && parts[1] === "") {
+    parts = ["1"];
+  }
+
+  console.log(parts);
 
   var i;
 
   for(i=0;i<parts.length;i++) {
+    // TODO, there's some serious duplication going on here
     if(/^(-|\d)/.test(parts[i])) {
       // Starts with a - or number, can't be a nick
       linesToGet.push(rangeParser.parse(parts[i]));
+    } else if(/^\/.*\/$/.test(parts[i])) {
+      // sandwitched between '/'s, it's a regex
+      try {
+        var regex = new RegExp(parts[i].substring(1, parts[i].length - 1));
+        linesToGet.push(regex);
+      } catch(ex) {
+        return cb("Could not parse regex: " + parts[i] + ", " + ex.toString());
+      }
     } else {
       // This is a nick. If the next one is a number then it's for this nick
       var lineObj = {
@@ -130,12 +144,23 @@ module.exports.getFormattedScrollbackLinesFromRanges = function(channel, ranges,
       if(/^(-|\d)/.test(parts[i+1])) {
         lineObj.lines = rangeParser.parse(parts[i+1]);
         i++; // We handled it :D
+      } else if(/^\/.*\/$/.test(parts[i+1])) {
+        // sandwitched between '/'s, it's a regex
+        try {
+          var regex = new RegExp(parts[i].substring(1, parts[i].length - 1));
+          lineObj.regex = regex;
+        } catch(ex) {
+          return cb("Could not parse regex: " + parts[i] + ", " + ex.toString());
+        }
+        i++;
       } else {
         lineObj.lines = [1];
       }
       linesToGet.push(lineObj);
     }
   }
+
+  console.log(linesToGet);
 
   var result = [];
   getCache(channel, function(cache) {
@@ -155,12 +180,31 @@ module.exports.getFormattedScrollbackLinesFromRanges = function(channel, ranges,
           }
           result.push(revCache[offset-1]);
         }
+      } else if(obj instanceof RegExp) {
+        var matches = revCache.filter(function(el) {
+          return obj.test(module.exports.formatLine(el));
+        });
+        if(matches.length === 0) {
+          return cb("Cannot find line matching regex " + obj.toString());
+        }
+        result.push(matches[0]);
       } else {
         // It's a nick
         var nick = obj.from;
         var nickCache = revCache.filter(function(el) {
           return el.from == nick;
         });
+
+        if(obj.regex) {
+          var matches = nickCache.filter(function(el) {
+            return obj.regex.test(module.exports.formatLine(el));
+          });
+          if(matches.length === 0) {
+            return cb("Cannot find line matching regex " + obj.toString());
+          }
+          result.push(matches[0]);
+          continue;
+        }
 
         for(var j=0; j < obj.lines.length; j++) {
           var offset = obj.lines[j];
