cafb37fab190d039bd8a5ef4c74097264b2ad3b8
Euan <euank@euank.com>
2015-11-06 21:53:23 -0800

    Move reddit-module to 'request' (https support)
    This allows 'https' urls to work; previously they did not.

--- modules/reddit/index.js
+++ modules/reddit/index.js
@@ -1,26 +1,23 @@
-var http = require('http');
+var request = require('request');
 
 var redditRE = /reddit\.com\/[^\s]+|redd\.it\//;
 
 function getCommentText(url, reply) {
   if(!redditRE.test(url)) return;
-	http.get(url + '.json', function(res) {
-		var data = '';
-		res.on('data', function(chunk) {
-			data += chunk;
-		});
-		res.on('end', function() {
-			try {
-				var json = JSON.parse(data);
-				var comment = json[1].data.children[0].data.body.replace(/\n/g, '\t');
-				
-				reply(comment.substring(0,500));
-			} catch(e) {
-        //console.log(e);
-				//return reply("Error handling response");
-			}
-		});
-	});
+
+  request.get(url + '.json', function(error, resp, body) {
+    if(error) {
+      reply("Could not get comment: " + error);
+    }
+    var data = '';
+    try {
+      var json = JSON.parse(body);
+      var comment = json[1].data.children[0].data.body.replace(/\n+/g, ' | ');
+
+      reply(comment.substring(0,500));
+    } catch(e) {
+    }
+  });
 }
 
 module.exports.commands = {
