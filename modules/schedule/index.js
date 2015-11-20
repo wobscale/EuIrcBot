4b74580aff42e266af99088dc96bbdda565abc6a
Billy Rhoades <billyarhoades@gmail.com>
2015-11-19 19:35:59 -0600

    Fixed checking if digest exists.

--- modules/schedule/index.js
+++ modules/schedule/index.js
@@ -370,7 +370,7 @@ module.exports.commands = {
       if(Object.keys(schedules).length == 0)
         return reply("There are no schedules to delete.");
 
-      if(schedules[digest] == undefined)
+      if(digest in schedules)
         return reply("There is no schedule for " + digest);
 
       var s = schedules[digest];
