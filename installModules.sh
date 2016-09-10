692d5249e3b5090c002c26729d324f7acc8afaca
Euan Kemp <euank@euank.com>
2016-09-09 20:04:13 -0700

    Add error handling to installModules

--- installModules.sh
+++ installModules.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-BASEDIR=`pwd`;
-for f in `find . -mindepth 2 -maxdepth 3 -name "package.json" -and -not -path "./node_modules/*" -exec dirname {} \;`
-do
+set -e
+
+BASEDIR=`pwd`
+for f in `find . -mindepth 2 -maxdepth 3 -name "package.json" -and -not -path "./node_modules/*" -exec dirname {} \;`; do
+  echo "Installing modules for $f"
   cd "$BASEDIR/$f"
   npm install
 done
