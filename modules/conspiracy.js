edcbd185556d0a16d60c4eafce5512b3cafb00a4
Rhoades <billyarhoades@gmail.com>
2016-09-15 13:19:58 -0500

    Added some miscellaenous theories / conspirators.

--- modules/conspiracy.js
+++ modules/conspiracy.js
@@ -2,22 +2,28 @@ module.exports.command = "conspiracy";
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw) {
 
   var entities = [
-    'the government', 'the Jews', 'the Goys', 'lizard people', 'scientists', 'doctors'
+    'the government', 'the Jews', 'the Goys', 'lizard people', 'scientists', 'doctors',
+    'big pharma', 'Monsanto', 'the gays', 'FDA', 'IRS', 'the illuminati', 'Hillary Clinton'
   ];
 
   var objects = [
-    'jet fuel', 'steel beams', 'bigfoot', 'vaccines', 'autism', '9/11', 'thinking about things'
+    'jet fuel', 'steel beams', 'bigfoot', 'vaccines', 'autism', '9/11', 'thinking about things',
+    'DNC 2016', 'Bernie Sanders', 'chemtrails'
   ];
 
   var motivations = [
     "cover up -object-",
-    "distract from -entity-"
+    "distract from -entity-",
+    "poison -entity-",
+    "make -entity- infertile",
   ];
 
   var theories = [
     "-object- can't melt -object-",
     "-entity- created -object- to -motivation-",
-    "-object- was an inside job!"
+    "-object- was an inside job!",
+    "-object- is secretly run by -entity-",
+    "-entity- is a false front for -entity-"
   ];
 
   var conspiracy = theories[Math.floor(Math.random() * theories.length)];
