84e9f0b7c3c4a714201e71d8367107d2ac31548f
Euan Kemp <euank@euank.com>
2017-02-14 19:32:14 -0800

    Help I have no idea what I'm doing

--- modules/help.js
+++ modules/help.js
@@ -1,30 +1,11 @@
 module.exports.command = 'help';
-var bot;
-module.exports.init = function(b) { bot = b; };
 
 // TODO make this work
 module.exports.help = function(){
   return "Gives a list of commands and topics, or help about a specific command or topic. Usage: " + bot.config.commandPrefix + "help [<command or topic>]";
 };
 
-function getCommands(t) {
-  return Object.keys(t.getAllCommandFns());
-}
-
-function extraTopics() {
-  // TODO
-  return '';
-}
 
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw) {
-  if(parts.length === 0) {
-    var replyStr = 'Commands: ' + getCommands(this).join(', ');
-    var extra = extraTopics();
-    if(extra.length > 0) {
-      replyStr += ', Extra help topics: ' + extra.join(', ');
-    }
-    return reply(replyStr + '.');
-  } else {
-    // TODO
-  }
+  return reply("Coming Soon^TM");
 };
