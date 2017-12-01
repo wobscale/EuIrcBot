41273132429050198ebda3c23dd8ee3498bc5b40
iliana weller <ilianaw@buttslol.net>
2017-12-01 08:45:07 -0800

    [whereIsSuscdFrom] Fix a country name
    https://en.wikipedia.org/wiki/Bosnia_and_Herzegovina

--- modules/whereIsSuscdFrom.js
+++ modules/whereIsSuscdFrom.js
@@ -1,4 +1,4 @@
-countries = ['Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina', 'Burundi'];
+countries = ['Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina', 'Burundi'];
 
 module.exports.run_whereIsSuscdFrom = function (r, p, reply) {
   reply(countries[Math.floor(Math.random() * countries.length)]);
