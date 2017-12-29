e64cae5680bcfa1101cb3e4269da0c064e659b7d
gmackie <graham.mackie@gmail.com>
2017-12-29 08:11:11 -0800

    Refactor Amazon Module to scrape detail page using NodeJS-XRay

--- modules/amazon/index.js
+++ modules/amazon/index.js
@@ -1,60 +1,56 @@
-const amazon = require('amazon-product-api');
+const XRay = require('x-ray');
+const xray = XRay();
 
-let amazonClient = null;
-let log;
-
-module.exports.init = function (bot) {
-  log = bot.log;
-  bot.getConfig('amazon.json', (err, conf) => {
-    if (err) return log.error(`Unable to load amazon module: ${err}`);
-    try {
-      amazonClient = new amazon.createClient(conf);
-    } catch (ex) {
-      log.error(`Error loading amazon library: ${ex}`);
-    }
-  });
+const amazonUrlWhitelist = [
+  "amazon.com",
+  "amazon.de",
+  "amazon.co.uk",
+  "amazon.co.jp",
+  "amazon.cn",
+  "amzn.eu",
+  "amzn.asia",
+  "amzn.to",
+  "a.co/",
+];
+const productQuery = {
+  productName: "#productTitle"
+}
+const priceQuery = {
+  ourPrice: '#priceblock_ourprice',
+  dealPrice: '#priceblock_dealprice',
+  salePrice: '#priceblock_saleprice',
+  offerPrice: '.offer-price',
 };
 
-const extractAsin = function (url) {
-  let asin;
-  const amazonRegex = /(B[0-9]{2}[0-9A-Z]{7}|[0-9]{9}(?:X|[0-9]))/;
-  match = amazonRegex.exec(url);
-  if (match && match[1]) { asin = match[0]; }
-  return asin;
-};
-
-const generateErrorMessage = function (error) {
-  const err = error[0].Error[0];
-  let msg;
-  // AWS.InvalidParameterValue is returned when an prospective ASIN isn't actually a real ASIN, so we squash it
-  if (err.Code[0] != 'AWS.InvalidParameterValue') { msg = `Error: ${err.Message[0]}`; }
-  return msg;
-};
-
-const generateResponse = function (results) {
-  const attributes = results[0].ItemAttributes[0];
-  let msg = attributes.Title;
-  // Free Prime exclusives and sold out items do not have a price. If this occurs, simply return title
-  if (typeof attributes.ListPrice !== 'undefined') {
-    const price = attributes.ListPrice[0].FormattedPrice[0];
-    msg += `- [${price}]`;
+const isAmazonUrl = function(url) {
+  for (let amazUrl in amazonUrlWhitelist) {
+    if (url.includes(amazUrl)) return true;
   }
-  return msg;
+  return false;
 };
 
-module.exports.url = function (url, reply) {
-  if (amazonClient === null) return log.error('Unable to handle amazon url; lib not loaded');
-  const asin = extractAsin(url);
-  if (asin) {
-    const query = { itemId: asin };
-    amazonClient.itemLookup(query, (err, results) => {
-      let msg;
+module.exports.url = function(url, reply) {
+  if (isAmazonUrl(url)) {
+    const query = Object.assign({}, productQuery, priceQuery);
+    xray(url, query)(function(err, data) {
       if (err) {
-        msg = generateErrorMessage(err);
-      } else {
-        msg = generateResponse(results);
+        reply(err);
+        return;
       }
-      if (msg) return reply(msg);
+      if (typeof data.productName === "undefined") {
+        reply("Error: Failed to find product name");
+        return;
+      }
+
+      let productName = data.productName.replace('\n', ' ').trim();
+      let price = Object.keys(priceQuery).map(selector => data[selector]).filter(price => price !== undefined)[0]
+      let displayPrice;
+      if (price !== undefined) {
+        displayPrice = price;
+      } else {
+        displayPrice = "$??";
+      }
+      reply(`${productName} - [${displayPrice}]`);
     });
   }
 };
