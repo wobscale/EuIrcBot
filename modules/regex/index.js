13f56362ac3c33b5a38815f53f86294b93586398
Euan Kemp <euan.kemp@coreos.com>
2018-03-22 18:51:13 -0700

    modules/regex: lint

--- modules/regex/index.js
+++ modules/regex/index.js
@@ -5,17 +5,17 @@ function replaceAll(find, replace, str) {
   return str.replace(new RegExp(find, 'g'), replace);
 }
 
-module.exports.msg = function (text, from, reply, raw) {
-  match = replaceRegex.exec(text);
+module.exports.msg = function (text, from, reply) {
+  const match = replaceRegex.exec(text);
   if (match) {
     // Replace the last thing the person said, if anything
-    if (lastSaid[from] != undefined) {
-      let personLastSaid = lastSaid[from],
-        find = match[1],
-        replace = match[2];
+    if (lastSaid[from] !== undefined) {
+      const personLastSaid = lastSaid[from];
+      const find = match[1];
+      const replace = match[2];
       const fixed = replaceAll(find, replace, personLastSaid);
 
-      if (fixed != personLastSaid) {
+      if (fixed !== personLastSaid) {
         const response = `<${from}> ${fixed}`;
 
         reply(response);
