a6b7a7e0c191a68fabf276c9bbf6a18ee92fcfd6
Euan Kemp <euan.kemp@coreos.com>
2018-03-22 18:45:39 -0700

    modules/testling: lint

--- modules/testlog.js
+++ modules/testlog.js
@@ -1,11 +1,11 @@
 module.exports.name = 'sirc-testlog';
 
-module.exports.run_testlog = function (remainder, parts, reply, command, from, to, text, raw, regex) {
+module.exports.run_testlog = function (remainder, parts, reply) {
   this.log.info(parts);
   reply('logged at info');
 };
 
-module.exports.run_testlog2 = function (remainder, parts, reply, command, from, to, text, raw, regex) {
+module.exports.run_testlog2 = function (remainder, parts, reply) {
   const self = this;
   process.nextTick(() => {
     self.log.info(parts);
@@ -13,7 +13,7 @@ module.exports.run_testlog2 = function (remainder, parts, reply, command, from, 
   });
 };
 
-module.exports.run_testlog3 = function (remainder, parts, reply, command, from, to, text, raw, regex) {
+module.exports.run_testlog3 = function (remainder, parts, reply) {
   process.nextTick(() => {
     module.exports.log.info(parts);
     reply('logged in a callback via module');
@@ -26,7 +26,7 @@ module.exports.init = function () {
   l = this.log;
 };
 
-module.exports.run_testlog4 = function (remainder, parts, reply, command, from, to, text, raw, regex) {
+module.exports.run_testlog4 = function (remainder, parts, reply) {
   process.nextTick(() => {
     l.info(parts);
     reply('logged in a callback via a module-scoped variable');
