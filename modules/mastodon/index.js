wobscale/EuIrcBot

commit 623efb08db0bb95734cd14fee491897a398acb8a

url: consistently use 'new URL'
'new URL' and 'url.parse' handle different sets of urls.

Using them inconsistently results in various errors, such as the onUrl
module passing something along that doesn't parse with the other method,
or detection of bogus urls.

For a specific example:

'!g matthew 5:29' parses as valid with 'url.parse', but throws an error
with new URL, so before `onUrl` would pass that along as a url with bad
results.

using `url.parse` everywhere could solve this, but `new URL` is the
newer api, so I'd rather standardize on that.

modules/mastodon/index.js
@@ -32,7 +32,7 @@ const execSync = require('child_process').execSync;
 const htmlToTextMod = require('html-to-text');
 const robotsParser = require('robots-parser');
 const util = require('util');
-const urlMod = require('url');
+const { URL } = require('url');
 
 const robotsTxtCache = new Cacheman();
 
@@ -45,7 +45,7 @@ let request = null;
 function robotRequest(options, fn) {
   const url = options.url;
   const userAgent = moduleConfig != null ? moduleConfig.userAgent : 'EuIrcBot';
-  const robotsTxtUrl = urlMod.resolve(url, '/robots.txt');
+  const robotsTxtUrl = new URL('/robots.txt', url);
 
   function handle(robotsTxt) {
     const robots = robotsParser(robotsTxtUrl, robotsTxt);
@@ -261,7 +261,7 @@ function handleUrl(url, reply, verbose = false) {
    */
 
   robotRequest(
-    { url: urlMod.resolve(url, '/api/v1/timelines/public') },
+    { url: new URL('/api/v1/timelines/public', url) },
     (error, response, bodyRaw) => {
       let body;
 
@@ -275,9 +275,9 @@ function handleUrl(url, reply, verbose = false) {
 
       [...new Set(body.map((item) => {
         try {
-          return urlMod.parse(item.url).host;
+          return new URL(item.url).host;
         } catch (ex) {
-          log.warn(`url.parse threw: ${ex}`);
+          log.warn(`URL threw: ${ex}`);
           return '';
         }
       }))].forEach((hostname) => {
@@ -300,9 +300,9 @@ function handleUrl(url, reply, verbose = false) {
 module.exports.run = (remainder, parts, reply) => {
   let url;
   try {
-    url = urlMod.parse(remainder);
+    url = new URL(remainder);
   } catch (ex) {
-    log.warn(`url.parse threw: ${ex}`);
+    log.warn(`URL threw: ${ex}`);
     return;
   }
   if (!checkHostname(url.host)) {
@@ -319,9 +319,9 @@ module.exports.run = (remainder, parts, reply) => {
 module.exports.url = (urlStr, reply) => {
   let url;
   try {
-    url = urlMod.parse(urlStr);
+    url = new URL(urlStr);
   } catch (ex) {
-    log.warn(`url.parse threw: ${ex}`);
+    log.warn(`URL threw: ${ex}`);
     return;
   }
   if (checkHostname(url.host)) {

modules/onUrl.js
@@ -1,5 +1,5 @@
-let url = require('url'),
-  XRegExp = require('xregexp');
+const { URL } = require('url');
+const XRegExp = require('xregexp');
 
 let tokenSplitRegex = XRegExp('(?![-_])[\\p{Pc}\\p{Pd}\\pZ]+'),
   tokenTrimBase = '(?![/])\\pP+',
@@ -18,7 +18,7 @@ module.exports.msg = function (text, from, reply, raw) {
   text.split(tokenSplitRegex)
     .map((token) => {
       try {
-        return url.parse(token.replace(tokenTrimStart, '').replace(tokenTrimEnd, ''));
+        return new URL(token.replace(tokenTrimStart, '').replace(tokenTrimEnd, ''));
       } catch (_) {}
     })
     .filter(u => u && u.protocol && u.host)

modules/onUrl.test.js
@@ -18,13 +18,14 @@ describe('onUrl.msg', () => {
       ['IANNA lanana banana http://example.com/ ', ['http://example.com/']],
       ['Punctual clocks at http://clock.town/!', ['http://clock.town/']],
       ['https://witches.town/@iliana/5147866', ['https://witches.town/@iliana/5147866']],
-      ['https://oo-ee.ooo', ['https://oo-ee.ooo/']], // an artifact of url.parse
+      ['https://oo-ee.ooo', ['https://oo-ee.ooo/']],
       ['https://oo-ee.ooo/underscore_page.html', ['https://oo-ee.ooo/underscore_page.html']],
       ['http://example.com/?url=foo/bar=baz.~+waitbeep.', ['http://example.com/?url=foo/bar=baz.~+waitbeep']],
       ['file://thank-gosh-our-security-is-good.and-this-is-fine/', ['file://thank-gosh-our-security-is-good.and-this-is-fine/']],
       ['file:///thank-gosh-our-security-is-good.and-this-is-fine/', []],
       ['http://名がドメイン.com', ['http://xn--v8jxj3d1dzdz08w.com/']],
-      ['broken auth http://%@foo.bar/baz pfff', []],
+      ['broken auth http://%@foo.bar/baz pfff', ["http://%@foo.bar/baz"]],
+      ['!g matthew 5:29', []],
     ];
 
     testCases.forEach((c) => {
@@ -37,7 +38,11 @@ describe('onUrl.msg', () => {
 
       onUrl.msg(input, '#from', 'reply', 'raw');
 
-      assert.equal(spy.callCount, expectedUrls.length);
+      assert.equal(
+        spy.callCount,
+        expectedUrls.length,
+        `${input} should have ${expectedUrls.length} urls, was ${spy.callCount}`,
+      );
       const foundUrls = spy.getCalls().map(call => call.args[1][0]);
       assert.deepEqual(expectedUrls, foundUrls);
     });

commit 3ec1d04be4fe38976c380073240860965ce7b21b

modules/mastodon: fix url error
This error was introduced in 623efb08db0bb95734cd14fee491897a398acb8a.

The robotsTxtUrl variable must be a string for the sake of the cache and
requests, not a proper URL object.

This also includes some logging improvements.

modules/mastodon/index.js
@@ -43,9 +43,9 @@ let moduleConfig = null;
 let request = null;
 
 function robotRequest(options, fn) {
-  const url = options.url;
+  const url = options.url.toString();
   const userAgent = moduleConfig != null ? moduleConfig.userAgent : 'EuIrcBot';
-  const robotsTxtUrl = new URL('/robots.txt', url);
+  const robotsTxtUrl = new URL('/robots.txt', url).toString();
 
   function handle(robotsTxt) {
     const robots = robotsParser(robotsTxtUrl, robotsTxt);
@@ -94,7 +94,7 @@ function defaultConfig() {
 
 function writeConfig() {
   bot.writeDataFile('config.json', JSON.stringify(moduleConfig), (err) => {
-    if (err) { log.error(`error writing mastodon module data: ${err}`); }
+    if (err) { log.error(err, 'error writing mastodon module data'); }
   });
 }
 
@@ -219,7 +219,7 @@ module.exports.init = (b) => {
     const ver = require('../../package.json').version;
     userAgent += `/${ver}`;
   } catch (ex) {
-    log.error(`error getting package version: ${ex}`);
+    log.error(ex, 'error getting package version');
   }
 
   let userAgentUrl = (moduleConfig !== null ? moduleConfig.userAgentUrl
@@ -229,7 +229,7 @@ module.exports.init = (b) => {
     try {
       commitish = execSync('git rev-parse --short HEAD', { encoding: 'utf8' }).trim();
     } catch (ex) {
-      log.error(`error determining deployed commit: ${ex}`);
+      log.error(ex, 'error determining deployed commit');
     }
     userAgentUrl = userAgentUrl.replace(/{commitish}/g, commitish);
   }
@@ -243,7 +243,7 @@ module.exports.init = (b) => {
 function handleUrl(url, reply, verbose = false) {
   fetchResource(url, (error, record) => {
     if (error) {
-      log.info({ error, mastodonUrl: url });
+      log.info(error, { mastodonUrl: url });
       if (verbose) { reply(`error: ${error.message}`); }
     } else {
       formatRecord(record, s => reply.custom({ replaceNewlines: true }, s));